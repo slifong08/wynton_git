98621d1c1f6a983c3df8cf1771b967d46e349bb3
slifong08 <slfong08@gmail.com>
2022-11-30 12:31:51 -0800

    bash script to download ucsc executables

---
+++ download_ucsc.sh
@@ -0,0 +1,8 @@
+# 20221130
+# SarahFong
+
+# download UCSC executables
+    
+rsync -aP hgdownload.soe.ucsc.edu::genome/admin/exe/linux.x86_64.v385/wigToBigWig
+rsync -aP hgdownload.soe.ucsc.edu::genome/admin/exe/linux.x86_64.v385/bigWigSummary
+rsync -aP hgdownload.soe.ucsc.edu::genome/admin/exe/linux.x86_64.v385/bigWigAverageOverBed
